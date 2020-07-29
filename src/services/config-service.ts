a22b896b4701608cba6ad155eca12db530ac52a6
Mike <veldsinkie.mv@gmail.com>
2020-07-29 21:49:40 +0200

    Add initial backend structure

---
+++ src/services/config-service.ts
@@ -0,0 +1,14 @@
+import * as fs from "fs";
+import { autoInjectable } from "tsyringe";
+
+@autoInjectable()
+export class ConfigService<T> {
+    /**
+     * Load config from a filepath.
+     * @param path to file where config json file is specified.
+     */
+    loadConfigFromPath = (path: string): T => {
+        const buffer = fs.readFileSync(path);   
+        return JSON.parse(buffer.toString()) as T;
+    }
+}
---
+++ src/types/config.ts
@@ -0,0 +1,6 @@
+type Config = {
+    DiscordAccessKey: string;
+    DiscordSecretKey: string;
+    DiscordRedirectUrl: string;
+    DiscordScopes: string[];
+}
