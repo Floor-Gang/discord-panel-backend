7b6ede97127711fe43264a990d5ed6ddb10fb924
Elian0213 <elian0213@outlook.com>
2020-08-23 16:58:39 +0200

    Fix weird error checking in setRuleSettings

--- src/controllers/rulemanager-controller.ts
+++ src/controllers/rulemanager-controller.ts
@@ -19,12 +19,11 @@ export class RuleManagerController implements IController {
     const data: postRules = req.body;
     const response: { success: boolean } =  await this.RuleManagerService.setServerRules(data.rules, data.channel, req.user.ID)
 
-    if (response.success) {
-      return res.json(response)
-    } else {
+    if (!response.success) {
       res.status(401)
-      return res.json(response)
     }
+
+    return res.json(response)
   }
 
   // Get the channels that you can pick from :)
