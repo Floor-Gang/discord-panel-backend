74bcc38ad4d528d400a093c025b75f961f3bf5d6
Tholhuijsen, P.T.C.M <p.t.c.m.tholhuijsen@student.tue.nl>
2020-08-24 16:12:09 +0200

    Start modmail api

--- src/controllers/modmail-controller.ts
+++ src/controllers/modmail-controller.ts
@@ -11,15 +11,39 @@ export class ModmailController implements IController {
     this.ModmailService = ModmailService;
   }
 
-  testFunction = async (req, res) => {
-    return res.json(this.ModmailService.getModmails())
+  getActiveConversations = async (req, res: any) => {
+    return res.json(await this.ModmailService.activeConversations())
+  }
+
+  getAll = async (req, res: any) => {
+    return res.json(await this.ModmailService.allEntries(req.params.table))
+  }
+
+  getFullConversation = async (req, res: any) => {
+    return res.json(await this.ModmailService.fullConversation(req.params.conversationID))
+  }
+
+  getAllActiveOrInactive = async (req, res: any) => {
+    return res.json(await this.ModmailService.allEntriesBool(req.params.table, req.params.active))
+  }
+
+  getCategoryPermissions = async (req, res: any) => {
+    return res.json(await this.ModmailService.categoryPermissions(req.params.categoryID))
   }
 
   getRouter = (): Router => {
     const router = Router();
 
-    // Get all modmails
-    router.get('/get/all', this.testFunction);
+    // Get all active conversations
+    router.get("/conversations/active/get", this.getActiveConversations);
+    // Get all messages and attachments for a conversation
+    router.get("/conversation/:conversationID/full", this.getFullConversation);
+    // Get all entries from a table in the modmail schema
+    router.get("/:table/all", this.getAll);
+    // Get all active or inactive entries from a table in the modmail schema
+    router.get("/:table/:active/all", this.getAllActiveOrInactive);
+    // Get all active permissions for a category
+    router.get("/category/:categoryID/permissions", this.getCategoryPermissions);
   
     return router;
   }
--- src/services/modmail-service.ts
+++ src/services/modmail-service.ts
@@ -1,17 +1,169 @@
 import {autoInjectable, inject} from "tsyringe";
 import { Pool, Client } from "pg";
 
+class BaseError {
+  constructor () {
+    const error = Error.apply(this, arguments);
+
+    Object.defineProperty(error, 'message', {
+      get() {
+        return arguments
+      }
+    });
+
+    Error.captureStackTrace(error, BaseError);
+    return error;
+  }
+}
+
+class ValueError extends BaseError {
+  constructor (public wrong_value: string, public accepted_values: Array<string>) {
+    super();
+
+    Object.defineProperty(super(), 'name', {
+      get() {
+        return 'ValueError'
+      }
+    });
+  }
+}
+
 @autoInjectable()
 export class ModmailService {
   config: Config;
-  pool: Pool;
+  postgresTables: string[];
+  knex: typeof require;
 
   constructor(@inject("Config") config: Config) {
-    this.pool = new Pool(config.Database)
+    this.knex = require('knex')({
+      client: 'pg',
+      connection: config.Database
+    });
     this.config = config;
+    this.postgresTables = ["all_messages_attachments", "categories", 
+                           "conversations", "messages", "muted", "notes", 
+                           "permissions", "standardreplies"];
   }
 
-  getModmails = async () => {
-    return {}
+  // Do I use this?
+  // activeConversations = async () => {
+  // or do I use this?
+  async activeConversations(): Promise<any> {
+    return this.knex('modmail.conversations')
+     .where({'active': true})
+     .select('*')
+     .on('query-response', function(response: Promise<any>) {
+       return response
+     })
+     .then((response: Promise<any>) => {
+       return response
+     })
+     .catch((err: Error) => {
+       return err
+     })
+  }
+
+  async fullConversation(conversationID: bigint): Promise<any> {
+    let conversations: Array<object>;
+    conversations = await this.knex('modmail.conversations')
+     .select('conversation_id')
+     .catch((err: Error) => {
+      return err
+    })
+
+    let conversationIDs = new Array();
+    for (let convID of conversations) {
+      conversationIDs.push(Object.values(convID)[0]);
+    }
+
+    if (!conversationIDs.some(x => x === conversationID.toString())) {
+      return new ValueError(conversationID.toString(), conversationIDs)
+    }
+
+    return this.knex('modmail.conversations')
+     .join('modmail.all_messages_attachments', 'conversations.conversation_id', '=', 'all_messages_attachments.conversation_id')
+     .where({'conversations.conversation_id': conversationID.toString()})
+     .select('*')
+     .on('query-response', function(response: Promise<any>) {
+       return response
+     })
+      .then((response: Promise<any>) => {
+       return response
+     })
+      .catch((err: Error) => {
+       return err
+     })
+  }
+
+  async allEntries(table: string): Promise<any> {
+    if (!this.postgresTables.some(x => x === table)) {
+      return new ValueError(table, this.postgresTables)
+    }
+
+    return this.knex("modmail."+table)
+     .select('*')
+     .on('query-response', function(response: Promise<any>) {
+      return response
+    })
+     .then((response: Promise<any>) => {
+      return response
+    })
+     .catch((err: Error) => {
+      return err
+    })
+  }
+
+  async allEntriesBool(table: string, active: boolean): Promise<any> {
+    if (!this.postgresTables.some(x => x === table)) {
+      return new ValueError(table, this.postgresTables)
+    }
+
+    return this.knex("modmail."+table)
+     .where('active', active)
+     .select('*')
+     .on('query-response', function(response: Promise<any>) {
+      return response
+    })
+     .then((response: Promise<any>) => {
+      return response
+    })
+     .catch((err: Error) => {
+      return err
+    })
+  }
+
+  async categoryPermissions(categoryID: bigint): Promise<any> {
+    let categories: string[];
+    categories = await this.knex('modmail.categories')
+     .select('category_id')
+     .catch((err: Error) => {
+      return err
+    })
+
+    let categoryIDs = new Array();
+    for (let catID of categories) {
+      categoryIDs.push(Object.values(catID)[0]);
+    }
+
+    if (!categoryIDs.some(x => x === categoryID.toString())) {
+      return new ValueError(categoryID.toString(), categoryIDs)
+    }
+
+    return this.knex('modmail.categories')
+     .join('modmail.permissions', 'categories.category_id', '=', 'permissions.category_id')
+     .where({
+       'permissions.active': true,
+       'categories.category_id': categoryID.toString()
+      })
+     .select('*')
+     .on('query-response', function(response: Promise<any>) {
+      return response
+    })
+     .then((response: Promise<any>) => {
+      return response
+    })
+     .catch((err: Error) => {
+      return err
+    })
   }
 }
